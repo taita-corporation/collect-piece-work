b2c2a278065e6bcdda03864ff3234dd96cb9057e
Koki Shibata <>
2021-06-09 15:48:56 +0900

    linterとtailwindコンフィグ追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,46 @@
-require("dotenv").config()
+require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    siteTitle: "collect piece work",
-    siteTitleDefault: "ファッションを通してモノと人、人と人をつなぐ",
-    siteUrl: "https://shopify-demo.gatsbyjs.com",
-    hrefLang: "en",
+    siteTitle: 'collect piece work',
+    siteTitleDefault: 'ファッションを通してモノと人、人と人をつなぐ',
+    siteUrl: 'https://shopify-demo.gatsbyjs.com',
+    hrefLang: 'en',
     siteDescription:
-      "A Gatsby starter using the latest Shopify plugin showcasing a store with product overview, individual product pages, and a cart.",
-    siteImage: "/default-og-image.jpg",
-    twitter: "@gatsbyjs",
+      'A Gatsby starter using the latest Shopify plugin showcasing a store with product overview, individual product pages, and a cart.',
+    siteImage: '/default-og-image.jpg',
+    twitter: '@gatsbyjs',
   },
   flags: {
     FAST_DEV: true,
   },
   plugins: [
     {
-      resolve: "gatsby-source-shopify",
+      resolve: 'gatsby-source-shopify',
       options: {
         apiKey: process.env.SHOPIFY_API_KEY,
         password: process.env.SHOPIFY_SHOP_PASSWORD,
         storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
-        shopifyConnections: ["collections"],
+        shopifyConnections: ['collections'],
       },
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-gatsby-cloud",
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-postcss',
     // Add your Google Analytics ID to the .env file to enable
     // Otherwise, this plugin can be removed
     process.env.GOOGLE_ANALYTICS_ID && {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GOOGLE_ANALYTICS_ID,
+        ],
       },
     },
   ].filter(Boolean),
-}
+};
