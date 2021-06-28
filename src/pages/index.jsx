b6aaa5be67f81264d3193a302c44c9913cb83478
Koki Shibata <kenergyintense@gmail.com>
2021-06-28 18:00:49 +0900

    merge: slider

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { GatsbyImage, StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 import ProductListing from '../components/product-listing';
 import { Hero } from '../components/hero';
+import * as s from './index.module.less';
 
 export const query = graphql`
   query {
     shopifyCollection(handle: { eq: "frontpage" }) {
       products {
         ...ProductCard
+      }
+    }
+    datoCmsTopPage {
+      conceptContentNode {
+        id
+        childMarkdownRemark {
+          html
+        }
+      }
+      conceptImage {
+        alt
+        gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED, aspectRatio: 1.5, height: 300)
       }
     }
   }
@@ -18,7 +32,36 @@ export default function IndexPage({ data }) {
     <Layout>
       <div className="container">
         <Hero />
+
+        {/* Concept Section */}
+        <div className="relative">
+          <StaticImage layout="constrained" objectFit="cover" loading="eager" src="../../static/background.png" placeholder="tracedSVG" className={s.background} />
+          <div className={s.concept}>
+            <div className="relative">
+              <GatsbyImage
+                image={data.datoCmsTopPage.conceptImage.gatsbyImageData}
+                className={s.conceptImage}
+                style={{
+                  position: 'absolute',
+                  transform: 'translateY(-50%)',
+                }}
+              />
+            </div>
+            <div>
+              <div>collect piece work</div>
+              {/* eslint-disable react/no-danger */}
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: data.datoCmsTopPage.conceptContentNode.childMarkdownRemark.html,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        {/* Product Feature Section */}
         {/* <ProductListing products={data.shopifyCollection.products} /> */}
+        {/* Instagram Section */}
+        {/* Shop Section */}
       </div>
     </Layout>
   );
